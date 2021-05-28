5b32dfb3e39e39d5f1d16977154b0e0639295552
Jason <jie-email@jie-trancender.org>
2021-05-28 13:43:24 +0800

    feat: add skynet test

---
+++ examples/config.helloWorld.lua
@@ -0,0 +1,15 @@
+root = "./"
+
+DEBUG = true
+project_name = "examples"
+thread = 8
+logger = nil
+harbor = 0
+start = "helloWorld"
+bootstrap = "snlua bootstrap"
+luaservice = root.."service/?.lua;"..root.."skynet/service/?.lua;"..root..project_name.."/?.lua"
+lualoader = root.."skynet/lualib/loader.lua"
+cpath = root.."skynet/cservice/?.so;"..root.."cservice/?.so"
+lua_cpath = root.."luaclib/?.so;"..root.."skynet/luaclib/?.so"
+lua_path = root.."lualib/?.lua;"..root..project_name.."/?.lua;"..root.."skynet/lualib/?.lua;"..root.."skynet/lualib/skynet/?.lua"
+snax = root.."service/?.lua;"..root.."skynet/service/?.lua"
